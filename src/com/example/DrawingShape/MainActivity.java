1074c876e97b54e52e7db7f3a857fffa9854f32a
AlvinChiou <alvinhsian@gmail.com>
2014-03-25 21:43:09 +0800

    Initial commit

---
+++ src/com/example/DrawingShape/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.DrawingShape;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String msg = "";
+        switch (item.getItemId()){
+            case R.id.shape:
+                msg = getString(R.string.shape);
+                break;
+            case R.id.circle:
+                msg = getString(R.string.circle);
+                break;
+            case R.id.rectangle:
+                msg = getString(R.string.rectangle);
+                break;
+
+            case R.id.color:
+                msg = getString(R.string.color);
+                break;
+            case R.id.blue:
+                msg = getString(R.string.blue);
+                break;
+            case R.id.red:
+                msg = getString(R.string.red);
+                break;
+            case R.id.exit:
+                this.finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+}
