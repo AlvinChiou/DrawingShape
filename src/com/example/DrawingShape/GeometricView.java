2447e00f1a409a2efcd1ccefa3e48c77522d3d1f
AlvinChiou <alvinhsian@gmail.com>
2014-03-26 03:10:57 +0800

    Some Problem

--- src/com/example/DrawingShape/GeometricView.java
+++ src/com/example/DrawingShape/GeometricView.java
@@ -3,28 +3,39 @@ package com.example.DrawingShape;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.graphics.drawable.shapes.RectShape;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class GeometricView extends View{
-    private ShapeDrawable shapes;
+    private ShapeDrawable shapesRect;
+    private ShapeDrawable shapesCirc;
     public GeometricView(Context context) {
         super(context);
         makeShapes();
+        makeCircle();
     }
 
     public GeometricView(Context context, AttributeSet attrs) {
         super(context, attrs);
         makeShapes();
     }
+
     public void makeShapes(){
-        shapes = new ShapeDrawable(new RectShape());
-        shapes.setBounds(10,10,760,760);
+        shapesRect = new ShapeDrawable(new RectShape());
+        shapesRect.setBounds(10,10,760,760);
+
+    }
+    public void makeCircle(){
+        shapesCirc = new ShapeDrawable(new OvalShape());
+        shapesCirc.setBounds(10,10,760,760);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+        shapesRect.getPaint().setARGB(255,255,0,0); //Red
+        shapesRect.draw(canvas);
     }
+
 }
